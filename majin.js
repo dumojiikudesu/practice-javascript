2c47b8ecdf5767d5b7ede25c0ea7c865237af18a
dumojiikudesu <rai821go@gmail.com>
2024-09-19 05:18:27 +0000

    9_19 javaScriptの導入

---
+++ majin.js
@@ -0,0 +1,18 @@
+document.querySelector("h1").textContent = "大川雷護"
+document.querySelector("h2").textContent = "ookawa"
+
+//documentとは！！
+/* htmlファイルで書かれているコードを一つのオブジェクトとしてみなす。*/
+
+//DOM（ドム）
+/* ドキュメントオブジェクトモデル　（Document Object Model）の略で、
+Webページの要素やコンテンツなどをツリー構成で表現したデータモデル*/
+
+//上記コードの意味
+//document.querySerector("h1").textContent = "自分の名前"
+
+//ドキュメントの中の("h1")タグを探す
+//document.querySelector("h1")
+
+//h1タグの中身のtextを変える
+//.textContent= "自分の名前"
