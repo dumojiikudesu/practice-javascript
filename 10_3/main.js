3b61b4d4701df8578b59d27c79531d8e5b893de9
dumojiikudesu <rai821go@gmail.com>
2024-10-04 06:03:19 +0000

    10_4

--- 10_3/main.js
+++ 10_3/main.js
@@ -1,12 +1,11 @@
 //クリックされたら動作する
 
 function errorInput(){
+  console.log(document.getElementById("month").value)
+  console.log(document.getElementById("day").value)
+
   const month = document.getElementById("month").value;
-  const day = document.getElementById("day").value;
-  if(month<1 || month>12){
-    alert("適切な値ではありません");
-    return
-  }
+  const day = document.getElementById("day").value
 
   console.log("month",month,"day",day)
   
